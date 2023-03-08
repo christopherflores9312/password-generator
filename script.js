ceb9313986a48d4a1de3c0c83fb5ecb0fd7a72a4
christopherflores9312 <christopherflores9312@gmail.com>
2023-03-08 13:55:08 -0700

    commented starter code

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
-// Assignment Code
+// Select the Generate Password button
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
+// Function to write the generated password to the page
 function writePassword() {
+  // Generate a new password
   var password = generatePassword();
+  // Select the password text area
   var passwordText = document.querySelector("#password");
-
+  // Set the value of the password text area to the generated password
   passwordText.value = password;
-
 }
 
-// Add event listener to generate button
+// Add a click event listener to the Generate Password button
 generateBtn.addEventListener("click", writePassword);
